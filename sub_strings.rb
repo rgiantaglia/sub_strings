771edd0814db52c320e7a2b89ad4ab30eb208d39
rgiantaglia <102831244+rgiantaglia@users.noreply.github.com>
2022-07-15 16:42:18 -0400

    submit solution for sub_strings project

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,1 +1,13 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings (sentence, dictionary)
+    no_space_sentence = sentence.gsub(/\s+/, "").downcase
+
+    result = dictionary.map {|word| no_space_sentence.scan(word).length}
+    
+    zipped = dictionary.zip(result)
+    
+    hash = Hash[zipped].select {|k, v| v > 0}
+end
+
+puts substrings("Howdy partner, sit down! How's it going?",dictionary)
